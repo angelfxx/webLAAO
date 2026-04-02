a47e91b80a22435db48f206f433cfb8cfe2c29a2
Luis Angel <angelfxx97@gmail.com>
2026-04-02 17:39:15 -0600

    Se hizo la conexion a supabase y se agregaron funciones de quitar o agregar tasks

--- js/app.js
+++ js/app.js
@@ -28,27 +28,40 @@ button.addEventListener("click", () => {
  * Reads the text from the input, and if it's not empty,
  * adds it to the array, renders it, and resets the input value.
  */
-function addTask() {
+async function addTask() {
     const text = input.value
 
     if (text === "") return
 
-    tasks.push(text)
+    await supabase
+        .from("tasks")
+        .insert({ text });
 
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-
-    renderTasks()
-
+    getTasks();
     input.value = ""
 }
 
+async function getTasks() {
+    const { data, error } = await supabase
+        .from("tasks")
+        .select("*");//select * es para seleccionar todas las columnas de la tabla tasks
+
+    if (error) {
+        console.error("Error al obtener las tareas:", error);
+        return;
+    }
+
+    tasks = data;
+    renderTasks();
+}
 
 function renderTasks() {
     list.innerHTML = "" // Limpiamos la lista antes de volver a pintarla
+
     tasks.forEach(task => {
-        const listitem = document.createElement("li")
-        listitem.textContent = task
-        list.appendChild(listitem)
+        const listitem = document.createElement("li");
+        listItem.textContent = task.text;
+        list.appendChild(listItem);
     })
 }
 
@@ -57,3 +70,9 @@ function deleteTask() {
     tasks.pop() // Elimina el último elemento del arreglo
     renderTasks() // Volvemos a renderizar la lista
 }
+
+getTasks();
+
+const supabaseUrl = "https://uwxqjsggzobhziiepmli.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV3eHFqc2dnem9iaHppaWVwbWxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUwODM5MjcsImV4cCI6MjA5MDY1OTkyN30.R9G6oQQ_wIdznRaWF5ogv9L6gWUQMMfkh6sBvD_nJE0"
+const supabase = supabase.createClient(supabaseUrl, supabaseKey)
